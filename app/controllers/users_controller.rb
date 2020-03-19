eb4319fb919b39f348b594c82dfb1bf81cc4fac5
AnouarLdn <anouar.haidary@gmail.com>
2020-03-19 21:14:03 +0100

    User_show_view created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  def show
+    @user = User.find(params[:id])
+  end
 
+  def index
+    redirect_to index_path
+  end
+  def cu_admin
+    unless current_user.is_admin == true
+      flash[:alert] = "Vous n'etes pas l'admin de ce compte."
+      redirect_back(fallback_location: request.referer)
+    end
+  end
 end
- 
