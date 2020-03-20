696e517a9a50ff080d6ddb4dd07d5c729f535d1d
Akushon <alann.deleau@gmail.com>
2020-03-20 15:43:11 +0100

    Correcting conflict

--- app/controllers/attires_controller.rb
+++ app/controllers/attires_controller.rb
@@ -14,16 +14,42 @@ class AttiresController < ApplicationController
       flash[:alert] = "Erreur dans l'enregistrement.Ton produit a été refusé"
       render 'new'
     end
+  end
+  def show
+    @attire = Attire.find(params[:id])
+  end
 
-    def new
-      @category_array = Category.all.map { |category| [category.kind, category.id] }
-      @size_array = Size.all.map { |size| [size.measure, size.id] }
-      @colour_array = Colour.all.map { |colour| [colour.tint, colour.id] }
-      @fabric_array = Fabric.all.map { |fabric| [fabric.variety, fabric.id] }
-      @situation_array = Situation.all.map { |situation| [situation.scope, situation.id] }
-      @condition_array = Condition.all.map { |condition| [condition.state, condition.id] }
-      @brand_array = Brand.all.map { |brand| [brand.trademark, brand.id] }
-      #@owner_array = Owner.all.map { |owner| [owner.name, owner.id] }
+  def edit
+    @attire = Attire.find(params[:id])
+  end
+
+  def update
+    @attire = Attire.find(params[:id])
+
+    if @attire.update(title: params[:title], description: params[:description], price: params[:price])
+    redirect_to @attire
+    else
+      render :edit
     end
   end
+  
+  def new
+    @category_array = Category.all.map { |category| [category.kind, category.id] }
+    @size_array = Size.all.map { |size| [size.measure, size.id] }
+    @colour_array = Colour.all.map { |colour| [colour.tint, colour.id] }
+    @fabric_array = Fabric.all.map { |fabric| [fabric.variety, fabric.id] }
+    @situation_array = Situation.all.map { |situation| [situation.scope, situation.id] }
+    @condition_array = Condition.all.map { |condition| [condition.state, condition.id] }
+    @brand_array = Brand.all.map { |brand| [brand.trademark, brand.id] }
+    #@owner_array = Owner.all.map { |owner| [owner.name, owner.id] }
+  end
+
+  def destroy
+    @attire = Attire.find(params[:id])
+    @attire.destroy
+    redirect_to user_path(current_user.id)
+  end
+
+
+
 end
