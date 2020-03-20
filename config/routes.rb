3e2576ad193ab33f2dda99183f712ca68d727900
Maxime Besse <bessemaxime@gmail.com>
2020-03-20 14:20:49 +0100

    Resources Routes for attires

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@ Rails.application.routes.draw do
 
 
 
-  get 'attires/index'
-  get 'attires/create'
-  get 'attires/new'
-  post 'attires/create'
+
   devise_for :users
 
   get 'static_pages/index'
@@ -13,8 +10,10 @@ Rails.application.routes.draw do
   root :to => 'static_pages#index'
 
   resources :users, only: [:edit, :show]
+  resources :attires
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   ## >> We commented the route below to be allowed to run migrations (also check ./app/views/layout/application.html.erb) << ##
   # root to: "home#index"
 end
+ 
