dcd95057d5a53fc09260769da5d8e824fb135ad6
fbrgmn <frbergamini@gmail.com>
2020-03-24 16:26:44 +0100

    added stripe api

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-
-
-
   root :to => 'static_pages#index'
+
   get 'search', to:"users#search"
 
   resources :users, only: [:edit, :show]
@@ -13,6 +11,9 @@ Rails.application.routes.draw do
   resources :attires do
     resources :itempicture, only: [:create, :destroy]
   end
+
+  resources :charges, only: [:new, :create]
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   ## >> We commented the route below to be allowed to run migrations (also check ./app/views/layout/application.html.erb) << ##
