4545c9177a8e715738d3d15023a0c4fbcaa87b03
fbrgmn <frbergamini@gmail.com>
2020-03-26 15:39:00 +0100

    added counting badge when cart has items

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_devise_parameters, if: :devise_controller?
+  before_action :cart_number
 
   def configure_devise_parameters
     devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:avatar, :first_name, :last_name, :about_me, :city_id, :email, :password, :password_confirmation)}
@@ -9,5 +10,10 @@ class ApplicationController < ActionController::Base
   #source: https://www.rubydoc.info/github/plataformatec/devise/Devise%2FControllers%2FHelpers:sign_in_and_redirect
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || root_path #edit_user_registration_path
+  end
+
+  def cart_number
+    @cart_number = current_user.carts.length
+    puts "cart_number = #{@cart_number}".red
   end
 end
