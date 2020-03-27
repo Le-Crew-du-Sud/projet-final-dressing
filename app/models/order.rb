67bae71585ee27b2add6ed218885cccd83a0f87d
fbrgmn <frbergamini@gmail.com>
2020-03-27 05:59:02 +0100

    transfered view to order page fixed association models cart and order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,8 @@
 class Order < ApplicationRecord
 
   has_many :carts, foreign_key: "order_id"
+  has_many :customers, through: :carts
+  has_many :attires, through: :carts
 end
 
 =begin # table "orders"
